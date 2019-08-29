7a88a53a4aecd8ab516acd64f2385ba23164c28a
lisa stubbe <lisa.stubbe.0402@gmail.com>
2019-08-29 21:08:48 +0200

    add ip + mac address

--- RPI_detector.sh
+++ RPI_detector.sh
@@ -7,3 +7,5 @@
 #ip_addresses = (1 2)
 stream_raw=$(tcpdump -nn -c5 -i wlan0 -e)
 #echo $stream_raw
+ip_addr=$(grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'<<<"$stream_raw")
+mac_addr=$(grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'<<<"$stream_raw")
