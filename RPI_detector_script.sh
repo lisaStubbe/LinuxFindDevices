ff7d37781848adad115fbd65c2a1161156ef07a7
lisa stubbe <lisa.stubbe.0402@gmail.com>
2019-08-29 21:16:59 +0200

    add mapfile ip + mac

--- RPI_detector_script.sh
+++ RPI_detector_script.sh
@@ -3,3 +3,6 @@ stream_raw=$(tcpdump -nn -c5 -i wlan0 -e)
 
 ip_addr=$(grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'<<<"$stream_raw")
 mac_addr=$(grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'<<<"$stream_raw")
+
+mapfile -t ip_addresses < <(grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'<<<"$stream_raw")
+mapfile -t mac_addresses < <(grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'<<<"$stream_raw")
